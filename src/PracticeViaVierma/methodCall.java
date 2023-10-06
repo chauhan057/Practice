fcb026328130af084b794056bd0f07f55ca2350a
Chauhan057 <vbiselsc498@gmail.com>
2023-10-06 13:49:23 +0530

    testing

---
+++ src/PracticeViaVierma/methodCall.java
@@ -0,0 +1,15 @@
+package PracticeViaVierma;
+
+public class methodCall
+{
+    public static void main(String[] args) {
+ aothrClass ob=new aothrClass();
+ ob.a();
+    }
+}
+class  aothrClass{
+    public void a(){
+        System.out.println("Hello");
+    }
+
+}
